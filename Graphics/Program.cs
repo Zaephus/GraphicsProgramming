e6e298ec59972a2756924b7d6c2e9d47904a3ec6
Marc Rozendaal <rozendaalmarc@gmail.com>
2023-05-06 01:48:07 +0200

    Small fix

--- Graphics/Program.cs
+++ Graphics/Program.cs
@@ -4,3 +4,10 @@ using ZaephusEngine;
 using(Window window = new Window(800, 800, "ZaephusEngine")) {
     window.Run();
 }
+
+// TODO: Add Camera.
+// TODO: Add Component and GameObject System.
+// TODO: Add Transform Component.
+// TODO: Add Game Clock.
+// TODO: Fix shader file path in build.
+// TODO: Abstract rendering from Mesh class.
