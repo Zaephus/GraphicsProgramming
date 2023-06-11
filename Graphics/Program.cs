a4a918c6956e044619f09de33fab31deac54bbc6
Marc Rozendaal <rozendaalmarc@gmail.com>
2023-06-12 00:09:46 +0200

    Everything Procedural opdracht

--- Graphics/Program.cs
+++ Graphics/Program.cs
@@ -1,14 +1,3 @@
 
-// TODO: Fix Property and field naming.
-
-// AssignmentOne assignmentOne = new AssignmentOne();
-// assignmentOne.Run();
-
-// LoadedSphereTest sphereLoadingTest = new LoadedSphereTest();
-// sphereLoadingTest.Run();
-
-// LoadedObjectTest loadedObjectTest = new LoadedObjectTest();
-// loadedObjectTest.Run();
-
 TerrainTest terrainTest = new TerrainTest();
 terrainTest.Run();
